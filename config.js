492e7866883b9382e9877be22e7a62d54eb2dc25
Satya-Dev-Tech <103887470+Satya-Dev-Tech@users.noreply.github.com>
2024-02-20 20:31:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 module.exports.admin_id = [{ id: 123456789, username: "username" }];
-module.exports.bot_token = "Your Bot Token";
+module.exports.bot_token = "5902670670:AAHpUNaBfZkHt3YVLRMktf2qGvrgyPgLDsI";
